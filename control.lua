45dd0658acd17b8cf55b1ac8b765572c7b4107bd
magu5026 <magu5026@gmail.com>
2018-07-02 18:38:33 +0200

    0.16.5 - Pull request by "shanemadden"

--- control.lua
+++ control.lua
@@ -20,6 +20,18 @@ function ON_REMOVE(event)
 	on_remove(event)
 end
 
+function SCRIPT_RAISED_BUILT(event)
+	if event.created_entity and event.created_entity.valid then
+		ON_BUILT(event)
+	end
+end
+
+function SCRIPT_RAISED_DESTROY(event)
+	if event.entity and event.entity.valid then
+		ON_REMOVE(event)
+    end
+end 
+
 
 
 script.on_init(ON_INIT)
@@ -27,3 +39,5 @@ script.on_configuration_changed(ON_CONFIGURATION_CHANGED)
 script.on_event(defines.events.on_tick,ON_TICK)
 script.on_event({defines.events.on_built_entity,defines.events.on_robot_built_entity},ON_BUILT)
 script.on_event({defines.events.on_pre_player_mined_item,defines.events.on_robot_pre_mined,defines.events.on_entity_died},ON_REMOVE)
+script.on_event(defines.events.script_raised_built,SCRIPT_RAISED_BUILT)
+script.on_event(defines.events.script_raised_destroy,SCRIPT_RAISED_DESTROY)
